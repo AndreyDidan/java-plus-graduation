25b37a21eaaebfc7fc4e3a24ca07a05b34f2f75f
Andrey Didan <andrey_didan@mail.ru>
2025-06-24 15:15:11 +0300

    Подготавлваем проект

--- stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -1,36 +1,40 @@
 package ru.practicum.controller;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.CreateHitDto;
 import ru.practicum.ResponseHitDto;
 import ru.practicum.ResponseStatsDto;
+import ru.practicum.StatsClient;
 import ru.practicum.service.StatisticService;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
 @Slf4j
-@RequiredArgsConstructor
-public class StatsController {
+@AllArgsConstructor
+@RequestMapping("/")
+public class StatsController implements StatsClient {
 
     private final StatisticService statisticService;
+    final String JSON_FORMAT_PATTERN_TIME = "yyyy-MM-dd HH:mm:ss";
 
-    @GetMapping("/stats")
-    public List<ResponseStatsDto> getStats(@RequestParam String start,
-                                           @RequestParam String end,
-                                           @RequestParam(required = false, defaultValue = "") List<String> uris,
-                                           @RequestParam(required = false, defaultValue = "false") Boolean unique) {
+    @Override
+    public List<ResponseStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
         log.info("Получен запрос на получение статистики по посещениям");
-        return statisticService.get(start, end, uris, unique);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(JSON_FORMAT_PATTERN_TIME);
+        String startFormatted = start.format(formatter);
+        String endFormatted = end.format(formatter);
+
+        return statisticService.get(startFormatted, endFormatted, uris, unique);
     }
 
-    @PostMapping("/hit")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseHitDto createHit(@RequestBody @Valid CreateHitDto createHitDto) {
+    @Override
+    public ResponseHitDto createHit(CreateHitDto createHitDto) {
         log.info("Сохранение информации об обращении к эндпоинту");
         return statisticService.create(createHitDto);
     }
