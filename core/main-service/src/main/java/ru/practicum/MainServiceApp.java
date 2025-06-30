45e00ef9b3979925fb67a0d9696ba06f2adba0be
Andrey Didan <andrey_didan@mail.ru>
2025-07-01 02:57:17 +0300

    Направляю первую часть дипломного проекта

--- core/main-service/src/main/java/ru/practicum/MainServiceApp.java
+++ core/main-service/src/main/java/ru/practicum/MainServiceApp.java
@@ -2,8 +2,12 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableFeignClients(clients = {StatsClient.class})
 public class MainServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApp.class, args);
