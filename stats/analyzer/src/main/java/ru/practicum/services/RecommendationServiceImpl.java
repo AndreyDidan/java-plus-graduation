6273a072dca04b72a83d400e350a4395df5763f8
Andrey Didan <andrey_didan@mail.ru>
2025-08-24 22:22:12 +0300

    Замечание 4

--- stats/analyzer/src/main/java/ru/practicum/services/RecommendationServiceImpl.java
+++ stats/analyzer/src/main/java/ru/practicum/services/RecommendationServiceImpl.java
@@ -80,7 +80,7 @@ public class RecommendationServiceImpl implements RecommendationService {
         List<RecommendedEvent> predictedEvents = aggregatedScores.entrySet().stream()
                 .map(entry -> RecommendedEvent.builder()
                         .eventId(entry.getKey())
-                        .score(entry.getValue())
+                        .score(getPrediction(entry.getKey(), ratedEvents))
                         .build())
                 .sorted(Comparator.comparingDouble(RecommendedEvent::getScore).reversed())
                 .limit(request.getMaxResults())
